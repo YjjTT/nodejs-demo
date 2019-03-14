df41aacb5a0b744bd66a92bb1126c9cd11754944
YjjTT <yjjtt@tempot.com>
2019-03-14 11:00:05 +0800

    script 发送请求

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ var server = http.createServer(function (request, response) {
         var newAmount = amount - 1
         if(Math.random() > 0.5){
             fs.writeFileSync('./db', newAmount)
-            response.setHeader('Content-Type', 'image/jpeg')
+            response.setHeader('Content-Type', 'application/javascript')
             response.statusCode = 200
-            response.write(fs.readFileSync('./dog.jpeg'))
+            response.write('alert("我是pay")')
         }else{
             response.statusCode = 400
             response.write('fail')
