91e7e09e089a5ae5e10843535474bf7a6d1293b2
YjjTT <yjjtt@tempot.com>
2019-03-14 10:21:01 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.env.PORT || 8888
+
+if (!port) {
+    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+    process.exit(1)
+}
+
+var server = http.createServer(function (request, response) {
+    var parsedUrl = url.parse(request.url, true)
+    var pathWithQuery = request.url
+    var queryString = ''
+    if (pathWithQuery.indexOf('?') >= 0) {
+        queryString = pathWithQuery.substring(pathWithQuery.indexOf('?'))
+    }
+    var path = parsedUrl.pathname
+    var query = parsedUrl.query
+    var method = request.method
+
+    /******** 从这里开始看，上面不要看 ************/
+
+    console.log('含查询字符串的路径\n' + pathWithQuery)
+
+    if (path === '/') {
+        var string = fs.readFileSync('./index.html', 'utf8')
+        var amount = fs.readFileSync('./db', 'utf8')
+        string = string.replace('&&&amount&&&', amount)
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.end(string)
+    } else {
+        response.statusCode = 404
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.end()
+    }
+
+
+
+
+
+
+
+
+    /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
